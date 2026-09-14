51fd6baeeb49e81728922f185863925c19ce6cb2
MohamedElashri <git@elashri.com>
2026-09-14 08:38:05 +0200

    Add insult-zsh plugin to the core

--- zsh/core/30-plugins.zsh
+++ zsh/core/30-plugins.zsh
@@ -3,4 +3,4 @@
 # 40-oh-my-zsh.zsh filters this list against plugins actually installed on the
 # current machine, so missing optional plugins do not break shell startup.
 
-DOTFILES_OMZ_PLUGINS=(git zsh-autosuggestions you-should-use adguard-helper)
+DOTFILES_OMZ_PLUGINS=(git zsh-autosuggestions you-should-use adguard-helper insult-zsh)
