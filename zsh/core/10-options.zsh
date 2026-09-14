4f2a6d491d2fb2a3d62ff5a6eb4afaed2ef3fb14
MohamedElashri <git@elashri.com>
2026-09-14 08:34:15 +0200

    Update backup

--- zsh/core/10-options.zsh
+++ zsh/core/10-options.zsh
@@ -6,3 +6,11 @@
 # fi
 
 export RUN_QURAN_VERSE_ON_STARTUP="${RUN_QURAN_VERSE_ON_STARTUP:-true}"
+
+# History options. Oh My Zsh (lib/history.zsh) already enables: extended_history,
+# hist_expire_dups_first, hist_ignore_dups, hist_ignore_space, hist_verify, and
+# share_history (which implies inc_append_history). Only the extras live here.
+setopt bang_hist            # ! triggers history expansion
+setopt hist_find_no_dups    # don't repeat duplicates when searching (Ctrl-r)
+setopt hist_ignore_all_dups # drop duplicates anywhere, not just adjacent
+setopt hist_reduce_blanks   # collapse multiple blanks in stored entries
